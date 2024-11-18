e0e88a992b75b09d3a85544ad81b5f5f1003a242
arsch___ <arsalangamerz0786@gmail.com>
2024-11-18 10:03:14 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import discord
+from discord.ext import commands
+import os
 
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+# prefix command
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! Ping is {round(bot.latency * 1000)}")
+
+# slash command
+@bot.slash_command(name='ping')
+async def slash_ping(ctx):
+    await ctx.respond(f"Pong! Ping is {round(bot.latency * 1000)}")
+
+# let bot run
+bot.run(os.environ["TOKEN"])
